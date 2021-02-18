4770e365a09df4fe60803aecf0fa3810ea7c79b4
Daniel <dan20402@gmail.com>
2021-02-18 18:31:06 -0300

    more env variable setup

--- server.js
+++ server.js
@@ -8,10 +8,9 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/deputados', router);
+app.use('/', router);
 
 const PORT = process.env.PORT;
+//const PORT = 3000;
 
 app.listen(PORT, console.log(`Listening on port ${PORT}`));
-
-//Just filler comment
